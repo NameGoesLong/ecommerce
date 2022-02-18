43659aa8acc7581bd04c638c8881f7fe35fdbf32
NameGoesLong <zhangcheny99@gmail.com>
2022-02-18 02:17:52 -0800

    san check

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ import ProductScreen from './screens/ProductScreen'
 function App() {
   return (
     <BrowserRouter>
-      <Header />
+      <Header /> 
       <main className="py-3">
         <Container>
           <Routes>
